499605f9b58a1dc14750619d55aa3e6be57d1ac3
jirapasjipipob <49808228+jirapasjipipob@users.noreply.github.com>
2019-04-20 02:43:58 -0700

    v.0.1a - Initial release !!
    Designed by - Jirapas Jipipob
                         - Tunwa Satianrapapong
                         - Thanthub Jamvitheelert
                         - Boonprom Napapongsuriya
                         - Thummachart Sunksuwan
    Developer    - Jirapas Jipipob
                         - Tunwa Satianrapapong
                         - Thanthub Jamvitheelert
                         - Boonprom Napapongsuriya

---
+++ gelcompiler_0.1a.c
@@ -0,0 +1,154 @@
+/*
+ * This file is part of the GELCOMPILER distribution (https://github.com/jirapasjipipob/pengel_language).
+ * Copyright (c) 2019 PENGEL INTERACTIVE.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <stdio.h>
+#include <stdlib.h>
+//function main
+int main()
+{
+  while(1)
+  {
+    FILE *read; //read file variable
+    FILE *write; //write file variable
+    char fname[64]; //file name storage
+    char length[256]; //max length
+    printf("gelcompiler>>>");
+    scanf("%s", fname); //input file name
+    printf("compiling... %s\n", fname); //display file name
+    read = fopen(fname, "r"); //open file
+    write = fopen("output.html", "w"); //write file
+    int nt = 0; // New Technology Mode = off
+    for(int i=0; fgets(length, 256, read) != NULL; i++) //translation loop
+    {
+      //code
+      if (length[0] == 'n' && length[1] == 't' && length[2] == '[')
+      {
+        nt = 1;
+      }
+      else if(length[0] == ']' && length[1] == ';')
+      {
+        nt = 0;
+      }
+      else if (nt == 1) { //New Technology Mode On
+        fprintf(write, "%s", length);
+      }
+      else if (nt == 0) { //New Technology Mode Off
+        	//start
+        	if (length[0] == 'm' && length[1] == 'a' && length[2] == 'i' && length[3] == 'n' && length[4] == '{')
+          {
+            fprintf(write, "<script>\n");
+          }
+
+          else if (length[0] == '}' && length[1] == ';')
+          {
+            fprintf(write, "</script>\n");
+          }
+          //print > console.log
+          else if (length[0] == 'p' && length[1] == 'r' && length[2] == 'i' && length[3] == 'n' && length[4] == 't' && length[5] == '(')
+          {
+          	fprintf(write, "console.log("); //first part
+
+         		for(int z=6; length[z] != ')'; z++) //second part
+         		{
+          		fprintf(write, "%c", length[z]);
+       	 		}
+
+        	  fprintf(write, ");\n"); //third part
+     	 		}
+
+     	 		//stop > debugger
+      		else if (length[0] == 's' && length[1] == 't' && length[2] == 'o' && length[3] == 'p' && length[4] == ';')
+      		{
+        		fprintf(write, "debugger;\n");
+      	  }
+
+      		//show > alert
+      		else if (length[0] == 's' && length[1] == 'h' && length[2] == 'o' && length[3] == 'w' && length[4] == '(')
+          {
+         		fprintf(write, "alert("); //first part
+
+        		for(int z=5; length[z] != ')'; z++) //second part
+            {
+          		fprintf(write, "%c", length[z]);
+            }
+
+            fprintf(write, ");\n"); //third part
+          }
+
+      		//create; > var
+      		else if (length[0] == 'c' && length[1] == 'r' && length[2] == 'e' && length[3] == 'a' && length[4] == 't' && length[5] == 'e')
+      		{
+        		fprintf(write, "var"); //first part
+
+        		for(int z=6; length[z] != ';'; z++) //second part
+        		{
+          		fprintf(write, "%c", length[z]);
+        		}
+
+        		fprintf(write, "\n");
+      		}
+
+      		//cube(v) > v = v*v*v
+      		else if (length[0] == 'c' && length[1] == 'u' && length[2] == 'b' && length[3] == 'e' && length[4] == '(')
+      		{
+      			for(int z=5; length[z] !=')'; z++) //first part
+        		{
+          		fprintf(write, "%c", length[z]);
+        		}
+        		fprintf(write, " = ");
+        		for(int z=5; length[z] !=')'; z++) //second part
+       		  {
+        		  fprintf(write, "%c", length[z]);
+       		  }
+       		  fprintf(write, "*");
+       		  for(int z=5; length[z] !=')'; z++) //third part
+       		  {
+        		  fprintf(write, "%c", length[z]);
+       		  }
+       		  fprintf(write, "*");
+        	  for(int z=5; length[z] !=')'; z++) //fourth part
+       		  {
+        		  fprintf(write, "%c", length[z]);
+       		  }
+        	  fprintf(write, ";\n");
+      		}
+
+      		//square(v) > v = v*v
+      		else if (length[0] == 's' && length[1] == 'q' && length[2] == 'u' && length[3] == 'a' && length[4] == 'r' && length[5] == 'e' && length[6] == '(')
+      		{
+      			for(int z=7; length[z] !=')'; z++) //first part
+        		{
+          		fprintf(write, "%c", length[z]);
+        		}
+        		fprintf(write, " = ");
+        		for(int z=7; length[z] !=')'; z++) //second part
+        		{
+          		fprintf(write, "%c", length[z]);
+        		}
+        		fprintf(write, "*");
+        		for(int z=7; length[z] !=')'; z++) //third part
+        		{
+          		fprintf(write, "%c", length[z]);
+        		}
+        		fprintf(write, ";\n");
+      		}
+      }
+      //end code
+    }
+    fclose(read); //stop reading file
+    fclose(write); //stop writing file
+  }
+}
